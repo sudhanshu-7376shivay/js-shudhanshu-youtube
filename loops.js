0ad177a65c7897f3b0c1e029f80014f6c57a00d7
sudhanshu-7376shivay <sudhanshu737697@gmail.com>
2025-06-02 14:49:46 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -1,88 +1,88 @@
 
 
  let array =["flash","batman","ironman","spiderman"]
-// for(let i=0; i<array.length; i++) {
-//     const element = array[i];
+for(let i=0; i<array.length; i++) {
+    const element = array[i];
     
-//     console.log(element);
+    console.log(element);
     
-// }
-// let arr=0
-// while(arr<array.length){
-//     console.log(`value id ${array[arr]}`);
-//     arr = arr+1;
+}
+let arr=0
+while(arr<array.length){
+    console.log(`value id ${array[arr]}`);
+    arr = arr+1;
     
+}
+
+let score = 1;
+do{
+    console.log(`score is ${score}`);
+    score++
+    
+}while(score <= 10);
+/ for  of  loop
+const arr = [1,2,3,4,5]
+
+for(const num of arr){
+   // console.log(num);
+    
+}
+const greetings = "hello world!"
+for(const greet of  greetings) {
+    if(greet == " "){
+        continue;
+     }
+    console.log(`each car is  ${greet}`);
+    
+    
+}
+
+maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('usa',"united state of  america")
+map.set('Fr',"France")
+
+for(const key in map) {
+    console.log(key);
+it is n0ot iteratable
 // }
 
-// let score = 1;
-// do{
-//     console.log(`score is ${score}`);
-//     score++
-    
-// }while(score <= 10);
-/// for  of  loop
-// const arr = [1,2,3,4,5]
+console.log(map);
 
-// for(const num of arr){
-//    // console.log(num);
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
     
-// }
-// const greetings = "hello world!"
-// for(const greet of  greetings) {
-//     if(greet == " "){
-//         continue;
-//      }
-//     console.log(`each car is  ${greet}`);
+}
+const myObject = {
+    'game1': 'NFS',
+    'GAME2': 'Spiderman'
+
+}
+for (const [key,value] of myObject) {
+    console.log(key ,':-', value);    
+}
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+for(const key in myObject) {
+    console.log(myObject[key]);
     
+}
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
     
-// }
-
-// maps
-
-// const map = new Map()
-// map.set('IN',"India")
-// map.set('usa',"united state of  america")
-// map.set('Fr',"France")
-
-// for(const key in map) {
-//     console.log(key);
-//it is n0ot iteratable
-// // }
-
-// console.log(map);
-
-// for (const [key, value] of map) {
-//     console.log(key, ':-', value);
-    
-// }
-// const myObject = {
-//     'game1': 'NFS',
-//     'GAME2': 'Spiderman'
-
-// }
-// for (const [key,value] of myObject) {
-//     console.log(key ,':-', value);    
-// }
-// const myObject = {
-//     js: 'javascript',
-//     cpp: 'C++',
-//     rb: "ruby",
-//     swift: "swift by apple"
-// }
-// for(const key in myObject) {
-//     console.log(myObject[key]);
-    
-// }
-// for (const key in myObject) {
-//     console.log(`${key} shortcut is for ${myObject[key]}`);
-    
-// }
+}
 
 const coding  = ["ruby","java","python","rust"]
-// coding.forEach( function (item){
-//     console.log(item);
+coding.forEach( function (item){
+    console.log(item);
     
-// })
+})
 
 coding.forEach(printMe)
 
